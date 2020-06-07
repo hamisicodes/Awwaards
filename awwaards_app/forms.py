36ee9f87791ecb4450dbbde3faf4e6d48a9f71f3
hamisicodes <newhamisi@gmail.com>
2020-06-07 19:53:09 +0300

    Add userEdit form and ProfileEdit forms

--- awwaards_app/forms.py
+++ awwaards_app/forms.py
@@ -20,3 +20,13 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Passwords dont match')
     
         return cd['password2']
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','email')
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth','photo','bio')
