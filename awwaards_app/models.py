2af859b42d3775dc05ec46ad4bf785de672fc5f8
hamisicodes <newhamisi@gmail.com>
2020-06-08 11:00:34 +0300

    Add a Project model

--- awwaards_app/models.py
+++ awwaards_app/models.py
@@ -30,3 +30,30 @@ class  Profile(models.Model):
     def get_profile(cls,id):
         profile = cls.objects.get(id = id)
         return profile
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    author = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    publish_at = models.DateTimeField(auto_now_add=True)
+    link = models.URLField()
+
+    def save_project(self):
+        self.save()
+        
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def search_projects(cls, search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
+
+    class Meta:
+        ordering = ['publish_at']
